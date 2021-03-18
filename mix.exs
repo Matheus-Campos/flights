d2c575735656b9ad33a623fbf875eb713d4d962b
Matheus Campos da Silva <matheus.campos@compasso.com.br>
2021-03-18 18:32:39 -0300

    chore: add ex machina

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule Flights.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      elixirc_paths: elixirc_paths(Mix.env)
     ]
   end
 
@@ -22,9 +23,13 @@ defmodule Flights.MixProject do
   defp deps do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      { :elixir_uuid, "~> 1.2" }
+      { :elixir_uuid, "~> 1.2"},
+      {:ex_machina, "~> 2.7.0"}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
     ]
   end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
 end

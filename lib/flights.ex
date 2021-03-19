c8e517b4cb30caee321af32b01cde3e408446cb9
Matheus Campos da Silva <matheus.campos@compasso.com.br>
2021-03-18 21:12:26 -0300

    feat: add generate report in facade

--- lib/flights.ex
+++ lib/flights.ex
@@ -2,6 +2,7 @@ defmodule Flights do
   alias Flights.Users.Agent, as: UserAgent
   alias Flights.Bookings.Agent, as: BookingAgent
   alias Flights.Bookings.Create, as: CreateBooking
+  alias Flights.Bookings.Report
   alias Flights.Users.Create, as: CreateUser
 
   def start_agents() do
@@ -10,6 +11,10 @@ defmodule Flights do
   end
 
   defdelegate create_user(params), to: CreateUser, as: :call
+
   defdelegate create_booking(user_id, params), to: CreateBooking, as: :call
+
   defdelegate get_booking(booking_id), to: BookingAgent, as: :get
+
+  defdelegate generate_report(from_date, to_date), to: Report, as: :call
 end
